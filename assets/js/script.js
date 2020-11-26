970bd79da84502fec229fbad1ff5b0a0e50de2da
Mila Decker <milacdecker@gmail.com>
2020-11-26 18:22:17 -0500

    added api fetches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,79 @@
 const searchBtn = document.getElementById('search-btn');
+const apiKey = ''
 
 var getInput = function(event) {
     event.preventDefault();
     
     var cuisineInput = document.getElementById('cuisine-menu').value;
-    var proteinInput = document.getElementById('protein-menu').value;
     var dietInput = document.getElementById('diet-menu').value;
 
-    var ingredientsInput = document.getElementById('ingredients').value.trim().toLowerCase();
-    var ingredientsArr = ingredientsInput.replace(/,/g, '').split(' ');
+    var ingrInput = document.getElementById('ingredients')
+        .value
+        .trim()
+        .toLowerCase();
+    var ingrArr = [];
+    if (ingrInput) {
+        ingrArr.push(ingrInput.replace(/,/g, '').split(' '));
+    };
 
-    var restrictionsInput = document.querySelectorAll('input[name="restriction"]:checked');
-    var restrictionsArr = [];
-
-    restrictionsInput.forEach((checkbox) => {
-        restrictionsArr.push(checkbox.value);
+    var restrInput = document.querySelectorAll('input[name="restriction"]:checked');
+    var restrArr = [];
+    restrInput.forEach((checkbox) => {
+        restrArr.push(checkbox.value);
     });
 
+    getData(cuisineInput, dietInput, ingrArr, restrArr);
 };
+
+var getData = function(cuisine, diet, ingr, restr) {
+
+    var apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}`
+    var paraName = ['cuisine', 'diet', 'includeIngredients', 'intolerances'];
+    var paraValue = [cuisine, diet, ingr, restr];
+
+    // concatenates search parameters
+    for (let i = 0; i < paraValue.length; i++) {
+        if (paraValue[i].length > 0) {
+            apiUrl += `&${paraName[i]}=${paraValue[i]}`
+        }
+    }
+
+    fetch(apiUrl).then(function(response) {
+
+        if (response.ok) {
+            response.json().then(function(data) {
+
+                if (data.results.length > 0) {
+                    getRecipe(data);
+                } else {
+                    alert(`No recipes found!`) // switch to modals, enter fewer search parameters
+                }
+            });
+        } else {
+            alert(`Error: ${response.statusText}`) // switch to modals later
+        }
+    })
+    .catch(function(error) {
+        alert('Unable to load recipes.'); // switch to modals later
+    });
+};
+
+var getRecipe = function(recipe) {
+
+    var idArr = [];
+    for (i = 0; i < recipe.results.length; i++) {
+        idArr.push(recipe.results[i].id);
+    };
+
+    var recipeUrl = `https://api.spoonacular.com/recipes/informationBulk?apiKey=${apiKey}&ids=${idArr.join()}`;    
+    fetch(recipeUrl).then(function(response) {
+
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+            })
+        } 
+    })
+}
 
 searchBtn.addEventListener('click', getInput)
